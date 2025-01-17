8c02def7d8867bbd2a1ee7c8c2b00020c96e7775
BilalNaseemChikoo <bilalnaseem@brandver.se>
2025-01-17 13:26:59 +0500

    first

---
+++ 1/DogLauncher.java
@@ -0,0 +1,10 @@
+// The Dog Launcher class will test drive the dog class
+
+public class DogLauncher {
+    public static void main(String[] args) {
+        Dog.makeNoise(); // Java goes to the dog class and calls the makeNoise method
+    }
+}
+
+// in java any code you run must be associated with some class
+// A class that uses another class is sometimes called a "client" of that class
---
+++ 1/Dogtwo.java
@@ -0,0 +1,13 @@
+public class Dogtwo {
+
+    public int weightInPounds;
+
+    public void main(String[] args) {
+        if (weightInPounds < 10) {
+            System.out.println("yip!");
+        } else {
+            System.out.println("Bark!");
+        }
+        // System.out.println("Bark!");
+    }
+}
