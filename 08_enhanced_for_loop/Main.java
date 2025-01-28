be7dd00c245c0d7dc2449acd06e5a64b142cb134
BilalNaseemChikoo <bilalnaseem@brandver.se>
2025-01-28 13:39:03 +0500

    renamed folders

---
+++ 08_enhanced_for_loop/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] fruits = { "apple", "banana", "orange" };
+
+        // DataType VariableName : ArrayName
+        for (String i : fruits) {
+            System.out.println(i);
+
+        }
+
+    }
+}
