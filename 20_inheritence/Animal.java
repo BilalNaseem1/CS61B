16900434829ce12e33eca8519e98d5b7bd9d01c5
BilalNaseem1 <bilalnaseem1@hotmail.com>
2025-02-09 12:06:41 +0500

    inheritence ic

---
+++ 20_inheritence/Animal.java
@@ -0,0 +1,13 @@
+public class Animal {
+
+    boolean isAlive;
+
+    Animal() {
+        isAlive = true;
+    }
+
+    void eat() {
+        System.out.println("The Animal is Eating");
+    }
+
+}
---
+++ 20_inheritence/Main.java
@@ -0,0 +1,11 @@
+// Inheritence - One class can inherit attributes (data) and methods from another class
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        
+        System.out.println(dog.isAlive);
+
+        dog.eat();
+    }
+}
