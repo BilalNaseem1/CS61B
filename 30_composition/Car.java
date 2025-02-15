fafb341d734681d7f601578a12cac60a3df94a27
BilalNaseem1 <bilalnaseem1@hotmail.com>
2025-02-15 18:19:41 +0500

    composition incomplete

---
+++ 30_composition/Car.java
@@ -0,0 +1,12 @@
+public class Car {
+    String model;
+    int year;
+    Engine engine;
+
+    Car(String model, int year, Engine engine) {
+        this.model = model; 
+        this.year = year;
+        this.engine = engine;
+    }
+    
+}
---
+++ 30_composition/Main.java
@@ -0,0 +1,8 @@
+// Composition in Java is a design principle where one class contains an instance of another class, 
+// meaning the contained object cannot exist independently of the containing class. 
+// It represents a strong "has-a" relationship (i.e., the lifecycle of the contained object depends on the container).
+
+public class Main {
+
+    
+}
