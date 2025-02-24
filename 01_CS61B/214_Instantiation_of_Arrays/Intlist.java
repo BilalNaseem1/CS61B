f4de197f9adfb9764476bc293a8ead5c8c649b95
BilalNaseemChikoo <bilalnaseem@brandver.se>
2025-02-24 14:15:20 +0500

    intlist

---
+++ 01_CS61B/214_Instantiation_of_Arrays/Intlist.java
@@ -0,0 +1,15 @@
+public class Intlist {
+
+    public int first;
+    public Intlist rest;
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        arr[3] = 4;
+        arr[4] = 5;
+    }
+
+}
---
+++ 01_CS61B/214_Instantiation_of_Arrays/README.md
@@ -0,0 +1,12 @@
+![alt text](image.png)
+
+```java
+new int[]{0, 1, 2, 95, 4};
+```
+
+- This instantiates a new object i.e. creates 5 boxes each of size 32.
+
+![alt text](image-1.png)
+
+## List vs Array
+- A list can grow arbitrarily large. A java array has a fixed size.
